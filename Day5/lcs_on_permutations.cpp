0d3075c1da95f9b020ddc65f86d03fc8c57b2764
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-05-21 21:52:53 -0300

    Adds solution to lcs on permutations

--- Day5/lcs_on_permutations.cpp
+++ Day5/lcs_on_permutations.cpp
@@ -1,2 +1,47 @@
 // https://codeforces.com/gym/102951/problem/C
 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int MAX = 1e5+17;
+const int INF = 1e9+17;
+ 
+int a[MAX], b[MAX], arr[MAX];
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n; cin >> n;
+    map<int, int> pos;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        pos[a[i]] = i;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+        arr[i] = pos[b[i]];
+    }
+    
+    vector<int> dp(n+1, INF);
+    dp[0] = -INF;
+ 
+    for (int i = 0; i < n; i++) {
+       int l = upper_bound(dp.begin(), dp.end(), arr[i]) - dp.begin();
+       if (l != dp.size() && dp[l-1] < arr[i])
+           dp[l] = arr[i];
+    }
+ 
+    int ans = 0;
+    for (int l = 0; l <= n; l++) {
+        if (dp[l] < INF)
+            ans = l;
+    }
+    
+    cout << ans << '\n';
+ 
+    return 0;
+}
