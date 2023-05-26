98c6058b55420c1af5b430378a6a56bf8117f510
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-05-26 14:33:07 -0300

    Adds recursive version of Longest Common Subsequence

--- Day3/Iterative/longest_common_subsequence.cpp
+++ Day3/Iterative/longest_common_subsequence.cpp
@@ -1,2 +1,44 @@
 // https://leetcode.com/problems/longest-common-subsequence/description/
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 1017;
+const int INF = 1e9+17;
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int dp[MAX][MAX] = {};
+        int n = text1.size(), m = text2.size();
+        
+        text1 = "$" + text1;
+        text2 = "$" + text2;
+        
+        for (int ps = 1; ps <= n; ps++) {
+            for (int pt = 1; pt <= m; pt++) {
+                if (text1[ps] == text2[pt]) dp[ps][pt] = max({
+                    dp[ps][pt - 1],
+                    dp[ps - 1][pt],
+                    dp[ps - 1][pt - 1] + 1
+                });
+                else dp[ps][pt] = max(dp[ps][pt - 1], dp[ps - 1][pt]);
+            }
+        }
+        
+        return dp[n][m];
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    Solution sol = Solution();
+    string s1, s2; cin >> s1 >> s2;
+    
+    cout << sol.longestCommonSubsequence(s1, s2) << '\n';
+
+    return 0;
+}
--- Day3/Recursive/longest_common_subsequence.cpp
+++ Day3/Recursive/longest_common_subsequence.cpp
@@ -6,36 +6,49 @@ using namespace std;
 
 const int MAX = 1017;
 
-int dp[MAX][MAX], grid[MAX][MAX];
-string text1, text2;
-int szs, szt;
+class Solution {
+public:
+    int dp[MAX][MAX], grid[MAX][MAX];
+    string text1, text2;
+    int szs, szt;
+    
+    int solve(int i, int j) {
+        if (i >= szs || j >= szt) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        
+        if (grid[i][j] == 1) dp[i][j] = max({
+            solve(i, j + 1),
+            solve(i + 1, j),
+            solve(i + 1, j + 1) + 1
+        });
+        else dp[i][j] = max(solve(i, j + 1), solve(i + 1, j));
+        
+        return dp[i][j];
+    }
 
-int solve(int i, int j) {
-    if (i >= szs || j >= szt) return 0;
-    if (dp[i][j] != -1) return dp[i][j];
+    int longestCommonSubsequence(string text1, string text2) {
+        szs = text1.size(); szt = text2.size();
     
-    if (grid[i][j] == 1) dp[i][j] = max({solve(i, j + 1), solve(i + 1, j), solve(i + 1, j + 1) + 1});
-    else dp[i][j] = max(solve(i, j + 1), solve(i + 1, j));
-    
-    return dp[i][j];
-}
+        memset(dp, -1, sizeof(dp));
+        
+        for (int i = 0; i < szs; i++) {
+            for (int j = 0; j < szt; j++) {
+                if (text1[i] == text2[j]) grid[i][j] = 1;
+            }
+        }
+        
+        return solve(0, 0);
+    }
+};
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    cin >> text1 >> text2;
-    szs = text1.size(); szt = text2.size();
+    Solution sol = Solution();
+    string s1, s2; cin >> s1 >> s2;
     
-    memset(dp, -1, sizeof(dp));
-    
-    for (int i = 0; i < szs; i++) {
-        for (int j = 0; j < szt; j++) {
-            if (text1[i] == text2[j]) grid[i][j] = 1;
-        }
-    }
-    
-    cout << solve(0, 0) << '\n';
+    cout << sol.longestCommonSubsequence(s1, s2) << '\n';
 
     return 0;
 }
