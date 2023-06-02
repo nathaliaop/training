74e05a3da25f8585ee55bdae0fa2ef3d4d2fc10d
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-06-01 23:23:52 -0300

    Adds solution to Movie Festival

--- Day16/movie_festival.cpp
+++ Day16/movie_festival.cpp
@@ -1,2 +1,41 @@
 //  https://cses.fi/problemset/task/1629
 
+#include <bits/stdc++.h>
+#define mp make_pair
+#define pii pair<int, int>
+#define ff first
+#define ss second
+
+using namespace std;
+
+bool cmp(pii a, pii b) {
+    if (a.ss < b.ss) return true;
+    return false;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n; cin >> n;
+    vector<pii> v(n);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> v[i].ff >> v[i].ss;
+    }
+    
+    sort(v.begin(), v.end(), cmp);
+    
+    int ans = 1;
+    pii last = v[0];
+    for (int i = 1; i < n; i++) {
+        if (last.ss <= v[i].ff) {
+            ans++;
+            last = v[i];
+        }
+    }
+    
+    cout << ans << '\n';
+
+    return 0;
+}
--- Day16/tasks_and_deadlines.cpp
+++ Day16/tasks_and_deadlines.cpp
@@ -1,2 +1,34 @@
 // https://cses.fi/problemset/task/1630
 
+#include <bits/stdc++.h>
+#define mp make_pair
+#define pii pair<int, int>
+#define ff first
+#define ss second
+using ll = long long;
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n; cin >> n;
+    vector<pii> v(n);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> v[i].ff >> v[i].ss;
+    }
+    
+    sort(v.begin(), v.end());
+    
+    ll curr = 0, ans = 0;
+    for (int i = 0; i < n; i++) {
+        curr += v[i].ff;
+        ans += (v[i].ss - curr);
+    }
+    
+    cout << ans << '\n';
+
+    return 0;
+}
