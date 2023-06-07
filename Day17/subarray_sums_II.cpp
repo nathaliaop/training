9e2c784901d6ef46401bcdd0ed6abf72f442aa64
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-06-07 09:29:23 -0300

    Adds solution to Subarray Sums 2

--- Day17/subarray_sums_II.cpp
+++ Day17/subarray_sums_II.cpp
@@ -1,2 +1,31 @@
 // https://cses.fi/problemset/task/1661
 
+#include <bits/stdc++.h>
+using ll = long long;
+
+using namespace std;
+
+const int MAX = 2e5+10;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, x; cin >> n >> x;
+    ll arr[MAX], psum[MAX];
+    map<ll, int> freq = {};
+    ll ans = 0;
+    psum[0] = 0;
+    freq[0] = 1;
+    
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+        psum[i] = arr[i] + psum[i - 1];
+        ans += freq[psum[i] - x];
+        freq[psum[i]]++;
+    }
+    
+    cout << ans << '\n';
+    
+    return 0;
+}
