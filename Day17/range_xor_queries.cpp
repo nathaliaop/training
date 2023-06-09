ee570a6f8844605f9738089e0ae4c389f7492e3f
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-06-08 22:22:09 -0300

    Adds solution to Range XOR Queries

--- Day17/range_xor_queries.cpp
+++ Day17/range_xor_queries.cpp
@@ -1,2 +1,26 @@
 // https://cses.fi/problemset/task/1650
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, q; cin >> n >> q;
+    int arr[n + 1];
+    int psum[n + 1];
+    psum[0] = 0;
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+        psum[i] = arr[i] ^ psum[i - 1];
+    }
+    
+    while (q--) {
+        int a, b; cin >> a >> b;
+        cout << (psum[a - 1] ^ psum[b]) << '\n';
+    }
+
+    return 0;
+}
